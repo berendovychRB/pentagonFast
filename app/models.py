3217ec4a5d51817fef97ddd9058fb1dd4330b0ee
berendovychRB <xooom9k@gmail.com>
2021-07-23 15:34:49 +0300

    Connection Databases

---
+++ app/models.py
@@ -0,0 +1,14 @@
+import datetime
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    email = Column(String, index=True, unique=True)
+    name = Column(String)
+    hashed_password = Column(String)
+    is_admin = Column(Boolean)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.datetime.utcnow)

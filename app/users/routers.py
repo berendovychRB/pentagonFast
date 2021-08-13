521394d22e314eb07729c425fea21f638e5de2d9
berendovychRB <xooom9k@gmail.com>
2021-08-13 14:35:07 +0300

    feat(current_user) make current user as function

--- app/users/routers.py
+++ app/users/routers.py
@@ -2,11 +2,10 @@ from typing import List
 
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
 
 from app import database
 from app.users import schemas
-from app.users.services import UserService
+from app.users.services import UserService, get_current_user
 
 router = APIRouter(tags=["Users"])
 get_session = database.get_session
@@ -16,7 +15,7 @@ userService = UserService()
 @router.get("/user/all", response_model=List[schemas.User])
 def all_users(
     service: UserService = Depends(),
-    current_user: schemas.User = Depends(userService.get_current_user),
+    current_user: schemas.User = Depends(get_current_user),
 ):
     return service.get_users()
 
@@ -26,7 +25,7 @@ def update(
     id: int,
     request: schemas.UserCreate,
     service: UserService = Depends(),
-    # current_user: schemas.User = Depends(UserService.get_current_user),
+    current_user: schemas.User = Depends(get_current_user),
 ):
     return service.update_user(request, id)
 
@@ -35,7 +34,7 @@ def update(
 def get_user(
     id: int,
     service: UserService = Depends(),
-    # current_user: schemas.User = Depends(userService.get_current_user),
+    current_user: schemas.User = Depends(get_current_user),
 ):
     return service.get_user(id)
 
@@ -44,13 +43,13 @@ def get_user(
 def delete_user(
     id: int,
     service: UserService = Depends(),
-    # current_user: schemas.User = Depends(userService.get_current_user),
+    current_user: schemas.User = Depends(get_current_user),
 ):
     return service.delete_user(id)
 
 
 @router.get("/user/me/", response_model=schemas.User)
-def read_users_me(current_user: schemas.User = Depends(UserService.get_current_user)):
+def read_users_me(current_user: schemas.User = Depends(get_current_user)):
     return current_user
 
 
@@ -59,9 +58,9 @@ def login(
     request: OAuth2PasswordRequestForm = Depends(),
     service: UserService = Depends(),
 ):
-    return service.authentication(request)
+    return service.authentication(request.username, request.password)
 
 
 @router.post("/registration", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
-def sing_up(request: schemas.UserCreate, service: UserService = Depends()):
-    return service.registration(request)
+def sing_up(user: schemas.UserCreate, service: UserService = Depends()):
+    return service.registration(user)
--- app/users/services.py
+++ app/users/services.py
@@ -1,18 +1,16 @@
-from fastapi import Depends, HTTPException, status
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
+from jose import JWTError
 from sqlalchemy.orm import Session
 
-from app.config import settings
 from app.database import get_session
 from app.exceptions import (
     CredentialException,
-    IncorrectPasswordError,
     InvalidCredentialError,
     ShortPasswordError,
     UserNotFoundError,
 )
-from app.security import Hash, encode_jwt_header_auth
+from app.security import Hash, encode_jwt_header_auth, jwt_decode
 from app.users import models, schemas
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
@@ -23,78 +21,73 @@ class UserService:
         self.session = db
 
     def _get_user_by_id(self, id):
-        return self.session.query(models.User).filter(models.User.id == id).first()
-
-    def _get_user_by_email(self, email):
-        return self.session.query(models.User).filter(models.User.email == email).first()
-
-    def get_users(self):
-        users = self.session.query(models.User).order_by(models.User.id).all()
-        return users
-
-    def update_user(self, request: schemas.UserCreate, id: int):
-        user = self.session.query(models.User).filter(models.User.id == id)
-
-        if not user.first():
-            raise UserNotFoundError
-
-        user.update(request)
-        self.session.commit()
-
-        return "Successful Updated"
-
-    def get_user(self, id: int):
-        user = self._get_user_by_id(id)
-
+        user = self.session.query(models.User).filter(models.User.id == id).first()
         if not user:
             raise UserNotFoundError
         return user
+
+    def _get_user_by_email(self, email):
+        user = self.session.query(models.User).filter(models.User.email == email).first()
+        if not user:
+            raise InvalidCredentialError
+        return user
+
+    def get_users(self):
+        return self.session.query(models.User).order_by(models.User.id).all()
+
+    def update_user(self, user: schemas.UserCreate, id: int):
+        existing_user = self._get_user_by_id(id)
+
+        update_data = user.dict(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(existing_user, key, value)
+
+        self.session.commit()
+
+        return user
+
+    def get_user(self, id: int):
+        return self._get_user_by_id(id)
 
     def delete_user(self, id: int):
         user = self._get_user_by_id(id)
 
-        if not user:
-            raise UserNotFoundError
-
         user.delete(synchronize_session=False)
         self.session.commit()
-        return "Successful Deleted"
 
-    def get_current_user(self, token: str = Depends(oauth2_scheme)):
-
-        try:
-            data = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-            email: str = data.get("sub")
-            if email is None:
-                raise CredentialException
-            token_data = schemas.TokenData(email=email)
-        except JWTError:
-            raise CredentialException
-        user = self._get_user_by_email(token_data.email)
-        if user is None:
-            raise CredentialException
-        return user
-
-    def authentication(self, request):
-        user = self._get_user_by_email(request.username)
-        if not user:
-            raise InvalidCredentialError
-        if not Hash.verify(
-            request.password,
+    def authentication(self, email, password):
+        user = self._get_user_by_email(email)
+        Hash.verify(
+            password,
             user.hashed_password,
-        ):
-            raise IncorrectPasswordError
+        )
         access_token = encode_jwt_header_auth(data={"sub": user.email})
-        return {"access_token": access_token, "token_type": "bearer"}
+        return {"access_token": access_token}
 
-    def registration(self, request):
-        if len(request.hashed_password) < 8:
+    def registration(self, user: schemas.UserCreate):
+        if len(user.hashed_password) < 8:
             raise ShortPasswordError
-        data = request.dict()
-        data["hashed_password"] = Hash.bcrypt(request.hashed_password)
+        data = user.dict()
+        data["hashed_password"] = Hash.bcrypt(user.hashed_password)
         new_user = models.User(**data)
         self.session.add(new_user)
         self.session.commit()
         self.session.refresh(new_user)
 
         return new_user
+
+
+def get_current_user(session: Session = Depends(get_session), token: str = Depends(oauth2_scheme)):
+
+    try:
+        data = jwt_decode(token)
+        email: str = data.get("sub")
+        if email is None:
+            raise CredentialException
+        token_data = schemas.TokenData(email=email)
+    except JWTError:
+        raise CredentialException
+    user = session.query(models.User).filter(models.User.email == token_data.email).first()
+    if user is None:
+        raise CredentialException
+    return user
